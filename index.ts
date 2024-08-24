5299df37d20e8afb7167dc75f82b64f9d327fd78
niemal <niemal@gmx.com>
2024-08-24 17:11:29 +0300

    feat: basic build output on path /build-output

--- index.ts
+++ index.ts
@@ -3,11 +3,35 @@ import { Elysia, t } from "elysia";
 import crypto from "crypto";
 import bodyType from "./types-github.ts";
 
+let buildInformation:
+  | {
+      repository: (typeof bodyType)["repository"]["full_name"];
+      author: (typeof bodyType)["head_commit"]["author"];
+      message: string;
+      url: string;
+      hash: string;
+    }
+  | undefined = undefined;
+let allOut = "";
+let proc: ReturnType<typeof Bun.spawn> | null = null;
+
 new Elysia({
   serve: {
     hostname: config.hostname,
   },
 })
+  .get("/build-output", ({ set }) => {
+    set.status = 200;
+    set.headers = {
+      "Content-Type": "text/plain",
+    };
+
+    if (allOut) {
+      return JSON.stringify(buildInformation) + "\n\n\n" + allOut;
+    }
+
+    return "No output.";
+  })
   .post(
     config.basePath + "/",
     ({ body, request, set }) => {
@@ -37,19 +61,49 @@ new Elysia({
         config.refs[body.repository.full_name as keyof typeof config.refs];
 
       if (event && configData[body.ref] && configData[body.ref][event]) {
+        if (proc) {
+          proc.kill(130); // 130 SIGINT
+        }
+
+        body.head_commit;
+        buildInformation = {
+          repository: body.repository.full_name,
+          author: body.head_commit.author,
+          message: body.head_commit.message,
+          url: body.head_commit.url,
+          hash: body.head_commit.id,
+        };
+
         const commandAndArguments = configData[body.ref][event].exec;
-        const proc = Bun.spawn(commandAndArguments, {
+        proc = Bun.spawn(commandAndArguments, {
           cwd: configData[body.ref][event].cwd,
         });
 
         const responseOut = new Response(proc.stdout);
         const responseErr = new Response(proc.stderr);
-        responseOut.text().then((text) => {
-          console.log("[github-webhooks]", text);
-        });
-        responseErr.text().then((text) => {
-          console.log("[github-webhooks][error]", text);
-        });
+
+        allOut = "";
+
+        responseOut
+          .text()
+          .then((text) => {
+            const message = "[github-webhooks] " + text;
+            console.log(message);
+            allOut += message;
+          })
+          .finally(() => {
+            proc = null;
+          });
+        responseErr
+          .text()
+          .then((text) => {
+            const message = "[github-webhooks][error] " + text;
+            console.error(message);
+            allOut += message;
+          })
+          .finally(() => {
+            proc = null;
+          });
 
         set.status = 200;
         return "OK";
